54dafccb7a0249087640184e2622ae61d78d1b17
hunj <hunjaelee@protonmail.com>
2024-10-13 20:23:49 -0400

    register Job & JobSource to admin

--- app/jobs/admin.py
+++ app/jobs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Job, JobSource
 
-# Register your models here.
+
+class JobAdmin(admin.ModelAdmin):
+    pass
+
+
+class JobSourceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(JobSource, JobSourceAdmin)
